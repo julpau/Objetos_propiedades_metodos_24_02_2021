55c8abce387b028c9421d8d07984c6d5a2509d64
Hersain Lozada Hermosa <hersainlozada@gmail.com>
2021-03-06 21:20:48 -0500

    Objetos_propiedades_metodos_24_02_2021

---
+++ main.js
@@ -0,0 +1,25 @@
+const Contrusctores = new Object({
+    Contrusctores_obj : function(){
+        this.nombre;
+        this.edad;
+        return this;
+    }
+});
+const settters = new Object({
+    settter_obj : function(){
+        this.nombre;
+        this.edad;
+        return this;
+    }
+});
+
+const Contrusctores_obj = Contrusctores.Contrusctores_obj.bind({nombre:'Hersain', edad: 44});
+const obj = new Object();
+obj.__proto__.construnctor = Contrusctores_obj();
+obj.__defineSetter__('setNombre', function(val) { obj.__proto__.construnctor.nombre = val; });
+obj.__defineGetter__('getNombre', function() { return obj.__proto__.construnctor.nombre; });
+Object.seal(obj);
+
+obj.setNombre = "Andrea";
+console.log(obj.getNombre);
+console.log(obj);
